192e59596dcb8e07dff9b1b853d2ddec6d9913e1
Paul Bayer <Paul.Bayer@gleichsam.de>
2020-02-27 17:31:42 +0100

    docstring added

--- src/utils.jl
+++ src/utils.jl
@@ -4,6 +4,28 @@
 # Paul Bayer, 2020
 # license: MIT
 #
+"""
+    onthread(f::F, id::Int) where {F<:Function}
+
+Execute a function f on thread id.
+
+# Examples, usage
+
+```julia
+julia> using ThreadingExperiments, .Threads
+
+julia> onthread(threadid, 2)
+2
+
+julia> onthread(3) do; threadid(); end
+3
+
+julia> onthread(4) do
+           threadid()
+       end
+4
+```
+"""
 function onthread(f::F, id::Int) where {F<:Function}
     t = Task(nothing)
     @assert id in 1:nthreads() "thread $id not available!"
